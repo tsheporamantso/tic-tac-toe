37c0643d1b95354881f956b8f3a45c539179c3e3
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-05 14:04:01 +0200

    Add Player component and pass player name and symbol as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Player from "./components/Player"
 
 function App() {
   
-
   return (
-    <p>coming soon</p>
+   <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name="Player 1" symbol="X" />
+        <Player name="Player 2" symbol="O" />
+      </ol>
+    </div>
+   </main>
   )
 }
 
