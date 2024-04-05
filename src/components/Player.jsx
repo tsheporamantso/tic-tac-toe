89ae2702db3692d500bce5e9a93106d40ede7120
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-05 16:22:21 +0200

    Add onchange event listner and collect player name value entered by user

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
 
-const Player = ({name, symbol}) => {
+const Player = ({initialName, symbol}) => {
 
+  const [playerName, setPlayerName] = useState(initialName);  
   const [isEditing, setIsEditing] = useState(false);
 
   const handleClick = () => {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
     // console.log(isEditing)
   }
 
-  let playerName = <span className="player-name">{name}</span>
+  const handleChange = (e) => {
+    setPlayerName(e.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>
   // let btnCaption = 'Edit'
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
     // btnCaption = "Save"
   }
   return (
       <li>
         <span className="player">
-          {playerName}
+          {editablePlayerName}
           <span className="player-symbol">{symbol}</span>
         </span>
         <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
