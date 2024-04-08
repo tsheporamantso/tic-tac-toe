1718c2dd39bedde1080e42cea667d9d20ed8a6a6
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-08 15:06:32 +0200

    Use ternary expressionto alternate between player name and input field

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,34 +1,42 @@
 import React, { useState } from "react";
 
-const Player = ({initialName, symbol}) => {
-
-  const [playerName, setPlayerName] = useState(initialName);  
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleClick = () => {
     setIsEditing((editing) => !editing);
     // console.log(isEditing)
-  }
+  };
 
   const handleChange = (e) => {
     setPlayerName(e.target.value);
-  }
+  };
 
-  let editablePlayerName = <span className="player-name">{playerName}</span>
+  // let editablePlayerName =
   // let btnCaption = 'Edit'
 
-  if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
-    // btnCaption = "Save"
-  }
+  // if (isEditing) {
+  //   editablePlayerName =
+  //   btnCaption = "Save"
+  // }
   return (
-      <li>
-        <span className="player">
-          {editablePlayerName}
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
-      </li>
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
   );
 };
 
