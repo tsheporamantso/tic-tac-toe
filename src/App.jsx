94e112f280e3260535a876ecda82cb17d2eb0d81
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-10 20:04:15 +0200

    Import Log component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
+import Log from "./components/Log"
 
 function App() {
   const [activePlayer, setActivePlayer] = useState('X')
@@ -20,7 +21,7 @@ function App() {
       </ol>
       <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
     </div>
-      LOG
+      <Log />
    </main>
   )
 }
